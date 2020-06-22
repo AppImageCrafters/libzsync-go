f736685855136cab3da6f02e9aecc2f5f797a54f
Alexis Lopez Zubieta <contact@azubieta.net>
2020-06-22 17:39:49 -0500

    correct control header test

--- control/control_test.go
+++ control/control_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestLoadControl(t *testing.T) {
+func TestLoadControlHeader(t *testing.T) {
 	data := []byte(`zsync: 0.6.2
 Filename: Hello World-latest-x86_64.AppImage
 MTime: Fri, 08 May 2020 17:36:00 +0000
@@ -17,15 +17,15 @@ SHA-1: da7a3ee0ebb42db73f96c67438ff38c21204f676
 
 `)
 
-	control, _ := ParseControl(data)
-	assert.Equal(t, "0.6.2", control.Version)
-	assert.Equal(t, "Hello World-latest-x86_64.AppImage", control.FileName)
-	assert.Equal(t, "Fri, 08 May 2020 17:36:00 +0000", control.MTime)
-	assert.Equal(t, uint(2048), control.BlockSize)
-	assert.Equal(t, int64(4096), control.FileLength)
-	assert.Equal(t, uint(2), control.HashLengths.ConsecutiveMatchNeeded)
-	assert.Equal(t, uint(2), control.HashLengths.WeakCheckSumBytes)
-	assert.Equal(t, uint(5), control.HashLengths.StrongCheckSumBytes)
-	assert.Equal(t, "Hello World-latest-x86_64.AppImage", control.URL)
-	assert.Equal(t, "da7a3ee0ebb42db73f96c67438ff38c21204f676", control.SHA1)
+	controlHeader, _, _ := LoadControlHeader(data)
+	assert.Equal(t, "0.6.2", controlHeader.Version)
+	assert.Equal(t, "Hello World-latest-x86_64.AppImage", controlHeader.FileName)
+	assert.Equal(t, "Fri, 08 May 2020 17:36:00 +0000", controlHeader.MTime)
+	assert.Equal(t, uint(2048), controlHeader.BlockSize)
+	assert.Equal(t, int64(4096), controlHeader.FileLength)
+	assert.Equal(t, uint(2), controlHeader.HashLengths.ConsecutiveMatchNeeded)
+	assert.Equal(t, uint(2), controlHeader.HashLengths.WeakCheckSumBytes)
+	assert.Equal(t, uint(5), controlHeader.HashLengths.StrongCheckSumBytes)
+	assert.Equal(t, "Hello World-latest-x86_64.AppImage", controlHeader.URL)
+	assert.Equal(t, "da7a3ee0ebb42db73f96c67438ff38c21204f676", controlHeader.SHA1)
 }
