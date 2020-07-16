9f257299fecb15b49a8095229508e52269edce4f
Alexis Lopez Zubieta <contact@azubieta.net>
2020-07-16 12:36:09 -0500

    Move search reusable chunks async code into a method

--- zsync2.go
+++ zsync2.go
@@ -13,70 +13,71 @@ type ZSync2 struct {
 	checksumsIndex *index.ChecksumIndex
 }
 
-func (zsync *ZSync2) SearchReusableChunks(seed io.ReadSeeker) (chan chunks.ChunkInfo, error) {
-	seedSize, err := seed.Seek(0, io.SeekEnd)
+func (zsync *ZSync2) SearchReusableChunks(input io.ReadSeeker) (chan chunks.ChunkInfo, error) {
+	inputSize, err := input.Seek(0, io.SeekEnd)
 	if err != nil {
 		return nil, err
 	}
 
-	// reset file idx
-	_, _ = seed.Seek(0, io.SeekStart)
+	// reset file offset to 0
+	_, _ = input.Seek(0, io.SeekStart)
 
 	chunkChannel := make(chan chunks.ChunkInfo)
-	go func() {
-
-		nextStep := zsync.BlockSize
-
-		maxOffset := (seedSize / zsync.BlockSize) * zsync.BlockSize
-
-		buf := hasedbuffer.NewHashedBuffer(int(zsync.BlockSize))
-
-		for i := int64(0); i <= maxOffset; i += nextStep {
-			n, err := io.CopyN(buf, seed, nextStep)
-			if err == io.EOF {
-				zeroBuff := make([]byte, nextStep-n)
-				_, err = buf.Write(zeroBuff)
-			}
-
-			if err != nil {
-				return
-			}
-
-			weakSum := buf.RollingSum()
-			weakMatches := zsync.checksumsIndex.FindWeakChecksum2(weakSum)
-
-			if weakMatches != nil {
-				strongSum := buf.CheckSum()
-				strongMatches := zsync.checksumsIndex.FindStrongChecksum2(strongSum, weakMatches)
-				if strongMatches != nil {
-					for _, match := range strongMatches {
-						newChunk := chunks.ChunkInfo{
-							Size:         zsync.BlockSize,
-							Source:       seed,
-							SourceOffset: i,
-							TargetOffset: int64(match.ChunkOffset) * zsync.BlockSize,
-						}
-
-						// chop zero filled chunks at the end
-						if newChunk.TargetOffset+newChunk.Size > seedSize {
-							newChunk.Size = seedSize - newChunk.TargetOffset
-						}
-
-						chunkChannel <- newChunk
-					}
-
-					// consume entire block
-					nextStep = int64(zsync.BlockSize)
-					continue
-				}
-			}
-
-			// just consume 1 byte
-			nextStep = 1
-		}
-
-		close(chunkChannel)
-	}()
+	go zsync.searchReusableChunksAsync(0, inputSize, input, chunkChannel)
 
 	return chunkChannel, nil
+}
+
+func (zsync *ZSync2) searchReusableChunksAsync(begin int64, end int64, input io.ReadSeeker, chunkChannel chan chunks.ChunkInfo) {
+	nextStep := zsync.BlockSize
+
+	maxOffset := (end / zsync.BlockSize) * zsync.BlockSize
+
+	buf := hasedbuffer.NewHashedBuffer(int(zsync.BlockSize))
+
+	for i := begin; i <= maxOffset; i += nextStep {
+		n, err := io.CopyN(buf, input, nextStep)
+		if err == io.EOF {
+			zeroBuff := make([]byte, nextStep-n)
+			_, err = buf.Write(zeroBuff)
+		}
+
+		if err != nil {
+			break
+		}
+
+		weakSum := buf.RollingSum()
+		weakMatches := zsync.checksumsIndex.FindWeakChecksum2(weakSum)
+
+		if weakMatches != nil {
+			strongSum := buf.CheckSum()
+			strongMatches := zsync.checksumsIndex.FindStrongChecksum2(strongSum, weakMatches)
+			if strongMatches != nil {
+				for _, match := range strongMatches {
+					newChunk := chunks.ChunkInfo{
+						Size:         zsync.BlockSize,
+						Source:       input,
+						SourceOffset: i,
+						TargetOffset: int64(match.ChunkOffset) * zsync.BlockSize,
+					}
+
+					// chop zero filled chunks at the end
+					if newChunk.TargetOffset+newChunk.Size > end {
+						newChunk.Size = end - newChunk.TargetOffset
+					}
+
+					chunkChannel <- newChunk
+				}
+
+				// consume entire block
+				nextStep = int64(zsync.BlockSize)
+				continue
+			}
+		}
+
+		// just consume 1 byte
+		nextStep = 1
+	}
+
+	close(chunkChannel)
 }
